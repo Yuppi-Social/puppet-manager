903f8e31fdbb2c06f54598224d4b494211c139c3
dark1zinn <edilsonjuininho154@gmail.com>
2025-03-13 23:17:52 -0300

    🔧 Adds utilities to scroll and interact with an element

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './PuppetManager';
 export * from './types';
+export * from './utils';
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './autoScrollByDistance.util';
+export * from './autoScrollToBottom.util';
+export * from './autoScrollUntilElement.util';
+export * from './clickButtonWhenVisible.util';
+export * from './constants.util';
+export * from './getCssSelector.util';
